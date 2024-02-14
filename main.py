37458ee6ce04e24930b75521fcd111c6cf10c214
Dominic Bluawofogbe <bluawofogbedominic@gmail.com>
2024-02-14 12:21:55 +0000

    added ball and score tracking feature

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+import time
+
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -7,18 +11,39 @@ screen.bgcolor("black")
 screen.title("Ping Pong Game")
 screen.tracer(0)
 
-paddle_r = Paddle((-350, 0))
-paddle_l = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+r_paddle = Paddle((350, 0))
+ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
-screen.onkey(paddle_l.up, "Up")
-screen.onkey(paddle_l.down, "Down")
-screen.onkey(paddle_r.up, "w")
-screen.onkey(paddle_r.down, "s")
+screen.onkey(r_paddle.up, "Up")
+screen.onkey(r_paddle.down, "Down")
+screen.onkey(l_paddle.up, "w")
+screen.onkey(l_paddle.down, "s")
 
 game_is_on = True
 
 while game_is_on:
+    time.sleep(ball.ball_speed)
     screen.update()
+    ball.move_ball()
+
+    # Detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 330 or ball.distance(l_paddle) < 50 and ball.xcor() < -330:
+        ball.bounce_x()
+
+    # Detect when right paddle misses ball
+    if ball.xcor() > 380:
+        scoreboard.increment_l_point()
+        ball.reset_ball()
+
+    # Detect when left paddle misses ball
+    if ball.xcor() < -380:
+        scoreboard.increment_r_point()
+        ball.reset_ball()
+
 
 screen.exitonclick()
